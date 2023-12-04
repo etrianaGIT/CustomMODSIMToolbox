b2dde5a70ac4102c347e64ba6e3539333a2bf123
Enrique Triana <etriana@rti.org>
2023-12-04 14:46:51 -0700

    Add class for processing observed flows in a folder.  Run project enables only this function.

--- MODSIMToolbox/MainMODSIMRun/Program.cs
+++ MODSIMToolbox/MainMODSIMRun/Program.cs
@@ -8,6 +8,7 @@ using MODSIMModeling.DiversionRotation;
 using MODSIMModeling;
 using MODSIMModeling.ReservoirOps;
 using MODSIMModeling.Routing;
+using MODSIMModeling.Preprocessing;
 
 namespace MODSIMModeling.MainMODSIMRun
 {
@@ -17,6 +18,7 @@ namespace MODSIMModeling.MainMODSIMRun
         // declaring the plug-ins
         public static ReservoirLayers resTool;
         public static RoutingUtils routeTool;
+        public static ObservedFLowImport obsFlowImport;
         //public static EconoModeling econoTool;
 
         static void Main(string[] CmdArgs)
@@ -33,14 +35,15 @@ namespace MODSIMModeling.MainMODSIMRun
             //econoTool = new EconoModeling(ref myModel);
             //econoTool.messageOutRun += OnMessage;
 
-            routeTool = new RoutingUtils(ref myModel);
-            routeTool.messageOutRun += OnMessage;
+            //routeTool = new RoutingUtils(ref myModel);
+            //routeTool.messageOutRun += OnMessage;
 
-            resTool = new ReservoirLayers(ref myModel,saveXYRun: true);
-            resTool.messageOutRun += OnMessage;
+            //resTool = new ReservoirLayers(ref myModel,saveXYRun: true);
+            //resTool.messageOutRun += OnMessage;
 
-            //foreach (Node res in myModel.Nodes_Reservoirs)
-            //	res.m.min_volume = res.m.min_volume;// 0;
+            obsFlowImport = new ObservedFLowImport(ref myModel);
+            obsFlowImport.ImportTimeseries("C:\\Users\\etriana\\Research Triangle Institute\\USGS Coop Agreement - Documents\\Modeling\\Data\\gage_search");
+            XYFileWriter.Write(myModel, myModel.fname);
 
             Modsim.RunSolver(myModel);
 
---
+++ MODSIMToolbox/ReservoirOps/ObservedFlow.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using Csu.Modsim.ModsimIO;
+using Csu.Modsim.ModsimModel;
+
+using Microsoft.VisualBasic.FileIO;
+
+namespace MODSIMModeling.Preprocessing
+{
+    public class ObservedFLowImport
+    {
+        public delegate void ProcessMessage(string msg);  // delegate
+
+        public Model myModel;
+        private DateTime MODSIMIniDate;
+
+        public event ProcessMessage messageOutRun;     //event
+        
+        public ObservedFLowImport(ref Model m_Model)
+		{
+			myModel = m_Model;
+        }
+
+        public void ImportTimeseries(string dataFolder)
+        {
+            //Get the initial date
+            MODSIMIniDate = myModel.TimeStepManager.dataStartDate;
+
+            string[] files = Directory.GetFiles(dataFolder, "*.*", System.IO.SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string fileName = fileInfo.Name;
+                fileName = Path.GetFileNameWithoutExtension(fileName);
+                if (fileName.StartsWith("DV_"))
+                {
+                    fileName = fileName.Replace("DV_", "");
+                }
+                Link gageLink = myModel.FindLink(fileName);
+                if(gageLink != null)
+                {
+
+                    myModel.FireOnMessage($"INFO:\tFound link for gage {gageLink.name}. Processing data...");
+                    string filePath = fileInfo.FullName;
+                    var dataTable = new DataTable();
+
+                    using (var parser = new TextFieldParser(filePath))
+                    {
+                        parser.TextFieldType = FieldType.Delimited;
+                        parser.SetDelimiters(",");
+                        parser.HasFieldsEnclosedInQuotes = true;
+
+                        // Read the column names from the first line of the CSV file
+                        string[] columnNames = parser.ReadFields();
+                        int datetimeIndex = -1;
+                        int qIndex = -1;
+                        int index = 0;
+                        foreach (string columnName in columnNames)
+                        {
+                            
+                            if (columnName == "datetime")
+                            {
+                                dataTable.Columns.Add(columnName);
+                                datetimeIndex = index;
+                            }
+                            if ( columnName == "Q(cfs)")
+                            {
+                                dataTable.Columns.Add(columnName);
+                                qIndex = index;
+                            }
+                            index++;
+                        }
+
+                        // Read the data from the remaining lines of the CSV file
+                        if (datetimeIndex > -1 && qIndex > -1)
+                        {
+                            bool isFirstDate = true;
+                            while (!parser.EndOfData)
+                            {
+                                string[] fields = parser.ReadFields();
+                                string datetime = fields[datetimeIndex];
+                                int q = -999;
+                                if(fields[qIndex].ToString()!="")
+                                    q = (int) Math.Round(double.Parse(fields[qIndex].ToString())*myModel.ScaleFactor,0);
+                                if (isFirstDate && DateTime.Parse(datetime) > MODSIMIniDate)
+                                {
+                                    dataTable.Rows.Add(MODSIMIniDate, -999);
+                                }
+                                if (DateTime.Parse(datetime) >= MODSIMIniDate)
+                                    dataTable.Rows.Add(datetime, q);
+                                isFirstDate = false;
+                            }
+                        }
+                        else
+                        {
+                            myModel.FireOnError($"ERROR [Parsing csv file] Could not find datetime and Q(csv) columns for {gageLink.name}.");
+                            continue;
+                        }
+                    }
+
+                    gageLink.m.adaMeasured.dataTable = dataTable;
+                    gageLink.m.adaMeasured.VariesByYear = true;
+                    gageLink.m.adaMeasured.units = new ModsimUnits("cfs");
+
+                }
+            }
+        }
+
+    }
+
+}
